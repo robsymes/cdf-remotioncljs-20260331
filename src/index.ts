4a30c628d62dca0531bfd44c2ce16229c89892f6
Rob Symes <robsymes@gmail.com>
2026-03-31 12:15:42 +0000

    test4

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,9 @@
 
 // registerRoot(RemotionRoot);
 
-import "../public/js/main.js"
+// import "../public/js/main.js"
 
+import * as MyCljsApp from "../public/js/main.js";
+
+// Force the engine to recognize the module
+console.log("Loading CLJS bundle...", MyCljsApp);
