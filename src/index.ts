9de5896fd7dd882ef348de7b9200e6524f504b75
Rob Symes <robsymes@gmail.com>
2026-03-31 12:58:31 +0000

    test12

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-// import { registerRoot } from "remotion";
-// import { RemotionRoot } from "./Root";
-
-// registerRoot(RemotionRoot);
-
-// import "../public/js/main.js"
-
-import * as MyCljsApp from "../public/js/main.js";
-
-// Force the engine to recognize the module
-console.log("Loading CLJS bundle...", MyCljsApp);
+// src/index.ts
+import "../public/js/main.js"; 
+// ^ Ensure this path matches the :output-dir in shadow-cljs.edn
